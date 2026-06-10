2efe73f4ce1b2a8094c872a60ffd837c61d74013
janmi <janmik@student.agh.edu.pl>
2026-06-10 09:18:20 +0200

    poprawka

--- delete_comment.php
+++ delete_comment.php
@@ -2,11 +2,14 @@
 require_once __DIR__ . '/functions.php';
 require_admin();
 
-$userId = (int) ($_GET['id'] ?? 0);
+$commentId = (int) ($_POST['comment_id'] ?? 0);
+$topicId = (int) ($_POST['topic_id'] ?? 0);
 
-if ($userId > 0) {
-    ban_user($userId);
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && $commentId > 0) {
+    delete_forum_comment($commentId);
 }
 
-header('Location: ' . url_for('admin_panel'));
+$redirectUrl = $topicId > 0 ? forum_topic_url($topicId) : url_for('forum');
+
+header('Location: ' . $redirectUrl);
 exit;
